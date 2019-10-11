a2fa188c32eb87ef51950aab4459d1d561ffbba8
Александр Макаров <makarov.alxr@yandex.ru>
2019-10-12 00:38:10 +0700

    data from pandas

--- data/utils.py
+++ data/utils.py
@@ -1,6 +1,10 @@
 from numpy import zeros, arange, ndarray
 from numpy.random import randint
 from torch import Tensor, randn
+import pandas as pd
+
+from pathlib import Path
+from os import listdir
 
 
 def create_random_classes(batch_size: int, num_classes: int):
@@ -16,8 +20,22 @@ def create_random_classes(batch_size: int, num_classes: int):
     
     return Tensor(res_np)
 
+
 def add_noise(expr: ndarray, beta: float = 1.0):
     #TODO doc-string
     res = expr + randn(expr.shape).cuda() * beta
     
     return res
+
+
+def get_raw_data(directory: Path):
+    original_expr_df = pd.read_csv(directory.joinpath('ST1 - original_expression.csv'), index_col=0)
+    transfer_annot_df = pd.read_csv(directory.joinpath('ST2 - transfer_annotation.csv'), index_col=0)
+    transfer_annot_df = transfer_annot_df.drop_duplicates(subset='sample_ID')
+
+    expression = original_expr_df.to_numpy()
+    class_ohe = pd.get_dummies(transfer_annot_df['Init state']).to_numpy()
+    print('Expression shape:', expression.shape)
+    print('Class ohe shape:', class_ohe.shape)
+    
+    return expression, class_ohe
