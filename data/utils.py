2392e228ceba0d0f8b12ab3a2e8949401ebdd056
Александр Макаров <makarov.alxr@yandex.ru>
2020-02-12 21:56:06 +0700

    preprocessing as callable argument

--- data/utils.py
+++ data/utils.py
@@ -10,13 +10,13 @@ from os import listdir
 def create_random_classes(batch_size: int, num_classes: int):
     assert isinstance(batch_size, int), TypeError("batch size must be numeric, integer")
     assert batch_size > 0, ValueError("It can't be negative")
-    
+
     assert isinstance(num_classes, int), TypeError("num_classes muust be numeric, integer")
     assert num_classes > 0, ValueError("It can't be negative")
-    
+
     res_np = zeros((batch_size, num_classes))
     res_np[arange(batch_size), randint(0, num_classes, batch_size)] = 1.
-    
+
     return Tensor(res_np)
 
 
@@ -26,11 +26,13 @@ def add_noise(expr: ndarray, beta: float = 1.0):
     if cuda.is_available():
         rand_values = rand_values.cuda()
     res = expr + rand_values * beta
-    
+
     return res
 
 
-def get_raw_data(directory: Path):
+def get_raw_data(directory: Path, preprocessing=None):
+    '''preprocess - callable'''
+    assert callable(preprocessing) or preprocessing is None
     original_expr_df = pd.read_csv(directory.joinpath('ST1 - original_expression.csv'), index_col=0)
     transfer_annot_df = pd.read_csv(directory.joinpath('ST2 - transfer_annotation.csv'), index_col=0)
     transfer_annot_df = transfer_annot_df.drop_duplicates(subset='sample_ID')
@@ -38,5 +40,10 @@ def get_raw_data(directory: Path):
     expression = original_expr_df.to_numpy()
     class_ohe = pd.get_dummies(transfer_annot_df['Init state']).to_numpy()
     cell_type = pd.get_dummies(transfer_annot_df['Cell type']).to_numpy()
-    
+
+    if preprocessing:
+        expression = preprocessing(expression)
+        class_ohe = preprocessing(class_ohe)
+        cell_type = preprocessing(cell_type)
+
     return expression, class_ohe, cell_type
